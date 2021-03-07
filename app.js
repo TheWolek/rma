28d8e8bece57efe939faf42ba9e1b235e1cd95e7
TheWolek <pawelm916@wp.pl>
2021-03-07 22:07:33 +0100

    cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
+//controllers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
