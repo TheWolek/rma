f3612d6318df218ad9c8bc8bb40b596c15afb541
TheWolek <pawelm916@wp.pl>
2022-02-06 13:08:22 +0100

    warehouse item find by barcode method

--- warehouse/items.js
+++ warehouse/items.js
@@ -7,7 +7,8 @@ const connection = mysql.createConnection(creds)
 connection.connect()
 
 router.post("/", (req, res) => {
-    //recive barcode in format "ticket_id/name/category"
+    // recive barcode in format "ticket_id/name/category"
+    // return {inserted id, ticket id, shelve id}
     let data = req.body.barcode.split("/")
 
     let sql = `INSERT INTO items (name, category, ticket_id, shelve) VALUES ("${data[1]}", "${data[2]}", ${data[0]}, 0)`
@@ -19,7 +20,17 @@ router.post("/", (req, res) => {
 })
 
 router.get("/", (req, res) => {
+    // recive barcode in format "ticket_id/name/category"
+    // if nothing was found return 404 status
+    // return first result
+    let data = req.body.barcode.split("/")
 
+    let sql = `SELECT item_id, name, shelve, tags, category, ticket_id FROM items WHERE ticket_id = ${data[0]}`
+    connection.query(sql, function (err, rows) {
+        if (err) throw err;
+        if (rows.length == 0) return res.status(404).send()
+        res.status(200).json(rows[0])
+    })
 })
 
 router.put("/", (req, res) => {
