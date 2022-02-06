f077f94d07a15cc02d6866f00165c3dd0b5dbfd1
TheWolek <pawelm916@wp.pl>
2022-02-06 13:20:19 +0100

    warehouse item changeShelve method

--- warehouse/items.js
+++ warehouse/items.js
@@ -22,7 +22,7 @@ router.post("/", (req, res) => {
 router.get("/", (req, res) => {
     // recive barcode in format "ticket_id/name/category"
     // if nothing was found return 404 status
-    // return first result
+    // returns 200 with first result
     let data = req.body.barcode.split("/")
 
     let sql = `SELECT item_id, name, shelve, tags, category, ticket_id FROM items WHERE ticket_id = ${data[0]}`
@@ -33,8 +33,23 @@ router.get("/", (req, res) => {
     })
 })
 
-router.put("/", (req, res) => {
+router.put("/changeshelve", (req, res) => {
+    // recive barcode in format "ticket_id/name/category", destination shelve and current shelve
+    // if current and destiantion sheleve are equal returns 400
+    // if no rows were changes return 404
+    // returns 200 with ticket_id, new_shelve id
+    let ticket_id = req.body.barcode.split("/")[0]
+    let dest = req.body.new_shelve
+    let current = req.body.shelve
 
+    if (dest == current) return res.status(400).send()
+
+    let sql = `UPDATE items SET shelve = ${dest} WHERE ticket_id = ${ticket_id}`
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        if (result.changedRows == 0) return res.status(404).send()
+        res.status(200).json({ ticket_id: ticket_id, new_shelve: dest })
+    })
 })
 
 
