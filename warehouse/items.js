4422ec95703772662abc0deffa91b42b304d9af5
TheWolek <pawelm916@wp.pl>
2022-02-11 19:59:32 +0100

    warehouse items basic validation + regex on register new item

--- warehouse/items.js
+++ warehouse/items.js
@@ -10,14 +10,18 @@ connection.connect()
 router.post("/", (req, res) => {
     // recive barcode in format "ticket_id-name-category"
     // return {inserted id, ticket id, shelve id}
+    if (!req.body.barcode) return res.status(400).json({ "message": "pole barcode jest wymagane" })
+
+    const reg = /^(\d{1,})-([A-ż(),.]{1,})([1-9]{1,})-([A-z(),.]{1,})([1-9]{1,})$/
+    if (!reg.test(req.body.barcode)) return res.status(400).json({ "message": "nieprawidłowy format pola barcode" })
+
     let data = req.body.barcode.split("-")
 
     let sql = `INSERT INTO items (name, category, ticket_id, shelve) VALUES ("${data[1]}", "${data[2]}", ${data[0]}, 0)`
     connection.query(sql, function (err, result) {
-        if (err) throw err;
+        if (err) return res.status(500).json(err);
         res.status(200).json({ id: result.insertId, ticket_id: data[0], shelve: 0 })
     })
-
 })
 
 //check if item with specific ticket_id is registered in warehouse
@@ -25,11 +29,13 @@ router.get("/exists", (req, res) => {
     // recive barcode in format "ticket-id-name-category"
     // if nothing was found return 404 with found: False
     // returns 200 with found: True
+    if (!req.query.barcode) return res.status(400).json({ "message": "pole barcode jest wymagane" })
+
     let data = req.query.barcode.split("-")[0]
     let sql = `SELECT item_id FROM items WHERE ticket_id = ${data}`
 
     connection.query(sql, function (err, rows) {
-        if (err) throw err;
+        if (err) return res.status(500).json(err);
         if (rows.length == 0) return res.status(404).json({ found: false })
         return res.json({ found: true })
 
@@ -41,11 +47,13 @@ router.get("/", (req, res) => {
     // recive barcode in format "ticket_id-name-category"
     // if nothing was found return 404 status
     // returns 200 with first row
+    if (!req.query.barcode) return res.status(400).json({ "message": "pole barcode jest wymagane" })
+
     let data = req.query.barcode.split("-")[0];
     let sql = `SELECT item_id, name, shelve, category, ticket_id FROM items WHERE ticket_id = ${data}`
 
     connection.query(sql, function (err, rows) {
-        if (err) throw err;
+        if (err) return res.status(500).json(err);
         if (rows.length == 0) return res.status(404).send()
         res.status(200).json(rows[0])
     })
@@ -56,11 +64,13 @@ router.get("/shelve", (req, res) => {
     // recive shelve id in req.params
     // if nothing was found return 404
     // reutns 200 with array of all items in shelve
+    if (!req.query.shelve) return res.status(400).json({ "message": "pole shelve jest wymagne" })
+
     let shelve = req.query.shelve
     let sql = `SELECT ticket_id, name, category FROM items WHERE shelve = ${shelve}`
 
     connection.query(sql, function (err, rows) {
-        if (err) throw err;
+        if (err) return res.status(500).json(err);
         if (rows.length == 0) return res.status(404).send()
         res.status(200).json(rows)
     })
@@ -73,6 +83,10 @@ router.put("/changeshelve", (req, res) => {
     // if no rows were changes return 404
     // if number of rows changed is diffrent from number of barcodes return 404 with message
     // returns 200 with ticket_id, new_shelve id
+    if (!req.body.barcode) return res.status(400).json({ "message": "pole barcode jest wymagane" })
+    if (!req.body.new_shelve) return res.status(400).json({ "message": "pole new_shelve jest wymagane" })
+    if (!req.body.shelve) return res.status(400).json({ "message": "pole shelve jest wymagane" })
+
     let ticket_id_arr = req.body.barcodes.map((el) => {
         return el.split("-")[0]
     })
