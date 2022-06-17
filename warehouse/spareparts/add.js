fa371d6f6752c7e495ec1f58ade6dabb3e320b70
TheWolek <pawelm916@wp.pl>
2022-06-17 23:42:02 +0200

    change add part to warehouse input type to array

--- warehouse/spareparts/add.js
+++ warehouse/spareparts/add.js
@@ -1,66 +1,99 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mysql = require('mysql')
-const creds = require('../../db_creds')
-const connection = mysql.createConnection(creds)
+const mysql = require("mysql");
+const creds = require("../../db_creds");
+const connection = mysql.createConnection(creds);
 
-connection.connect()
+connection.connect();
 
 // register new category of sparepart
 router.post("/new", (req, res) => {
-    // recive {"name": string, "category": string, "producer": string}
-    // return 400 if any of parameters is missig OR is empty OR does not match regEx
-    // return 500 if there was DB error
-    // return 200 with {"id": INT}
+  // recive {"name": string, "category": string, "producer": string}
+  // return 400 if any of parameters is missig OR is empty OR does not match regEx
+  // return 500 if there was DB error
+  // return 200 with {"id": INT}
 
-    // if (!req.body.sn) return res.status(400).json({ "message": "pole sn jest wymagane" })
-    if (!req.body.name) return res.status(400).json({ "message": "pole name jest wymagane" })
-    if (!req.body.category) return res.status(400).json({ "message": "pole category jest wymagane" })
-    if (!req.body.producer) return res.status(400).json({ "message": "pole producer jest wymagane" })
+  // if (!req.body.sn) return res.status(400).json({ "message": "pole sn jest wymagane" })
+  if (!req.body.name)
+    return res.status(400).json({ message: "pole name jest wymagane" });
+  if (!req.body.category)
+    return res.status(400).json({ message: "pole category jest wymagane" });
+  if (!req.body.producer)
+    return res.status(400).json({ message: "pole producer jest wymagane" });
 
-    const regName = /^([A-ż]{1,})([,. ()0-9'"-]){0, }$/
-    // const regSN = /^([0-9-]{1,})$/
-    const regCatProd = /^([A-ż]{1,})([,. ()0-9'"-]){0, }$/
+  const regName = /^([A-ż]{1,})([,. ()0-9'"-]){0, }$/;
+  // const regSN = /^([0-9-]{1,})$/
+  const regCatProd = /^([A-ż]{1,})([,. ()0-9'"-]){0, }$/;
 
-    if (!regName.test(req.body.name)) return res.status(400).json({ "message": "nieprawidłowy format pola name" })
-    // if (!regSN.test(req.body.sn)) return res.status(400).json({ "message": "nieprawidłowy format pola sn" })
-    if (!regCatProd.test(req.body.category)) return res.status(400).json({ "message": "nieprawidłowy format pola category" })
-    if (!regCatProd.test(req.body.producer)) return res.status(400).json({ "message": "nieprawidłowy format pola producer" })
+  if (!regName.test(req.body.name))
+    return res.status(400).json({ message: "nieprawidłowy format pola name" });
+  // if (!regSN.test(req.body.sn)) return res.status(400).json({ "message": "nieprawidłowy format pola sn" })
+  if (!regCatProd.test(req.body.category))
+    return res
+      .status(400)
+      .json({ message: "nieprawidłowy format pola category" });
+  if (!regCatProd.test(req.body.producer))
+    return res
+      .status(400)
+      .json({ message: "nieprawidłowy format pola producer" });
 
-    let sql = `INSERT INTO spareparts_cat (name, category, producer) VALUES ('${req.body.name}','${req.body.category}','${req.body.producer}')`
+  let sql = `INSERT INTO spareparts_cat (name, category, producer) VALUES ('${req.body.name}','${req.body.category}','${req.body.producer}')`;
 
-    connection.query(sql, function (err, result) {
-        if (err) {
-            // if (err.code == "ER_DUP_ENTRY") return res.status(400).json({ "message": "część z podanym SN została już zarejestrowana" })
-            return res.status(500).json(err)
-        }
-        res.status(200).json({ "id": result.insertId })
-    })
-})
+  connection.query(sql, function (err, result) {
+    if (err) {
+      // if (err.code == "ER_DUP_ENTRY") return res.status(400).json({ "message": "część z podanym SN została już zarejestrowana" })
+      return res.status(500).json(err);
+    }
+    res.status(200).json({ id: result.insertId });
+  });
+});
 
 // add part to warehouse
 router.post("/", (req, res) => {
-    // recive {"cat_id": INT, "amount": INT, "shelve": INT}
-    // return 400 if any of parameters is missing OR is empty OR does not match regEx
-    // return 500 if there was DB error
-    // return 200 with {"part_id": INT}
+  // recive [{"cat_id": INT, "amount": INT, "shelve": INT}, ...]
+  // return 400 if array is empty
+  // return 400 if any of parameters is missing OR is empty OR does not match regEx
+  // return 500 if there was DB error
+  // return 200 with {"part_id": INT}
 
-    if (!req.body.cat_id) return res.status(400).json({ "message": "pole cat_id jest wymagane" })
-    if (!req.body.amount) return res.status(400).json({ "message": "pole amount jest wymagane" })
-    if (!req.body.shelve) return res.status(400).json({ "message": "pole shelve jest wymagane" })
+  if (!req.body.length > 0)
+    return res.status(400).json({ message: "nie podano przedmiotów" });
 
-    const reg = /^([1-9]{1,})$/
+  req.body.forEach((el) => {
+    if (!el.cat_id)
+      return res.status(400).json({ message: "pole cat_id jest wymagane" });
+    if (!el.amount)
+      return res.status(400).json({ message: "pole amount jest wymagane" });
+    if (!el.shelve && el.shelve !== 0)
+      return res.status(400).json({ message: "pole shelve jest wymagane" });
+  });
 
-    if (!reg.test(req.body.cat_id)) return res.status(400).json({ "message": "nieprawidłowy format pola cat_id" })
-    if (!reg.test(req.body.amount)) return res.status(400).json({ "message": "nieprawidłowy format pola amount" })
-    if (!reg.test(req.body.shelve)) return res.status(400).json({ "message": "nieprawidłowy format pola shelve" })
+  const reg = /^([1-9]{1,})$/;
+  const regShelve = /^([0-9]{1,})$/;
 
-    let sql = `insert into spareparts (cat_id, amount, shelve) values (${req.body.cat_id}, ${req.body.amount}, ${req.body.shelve})`
+  req.body.forEach((el) => {
+    if (!reg.test(el.cat_id))
+      return res
+        .status(400)
+        .json({ message: "nieprawidłowy format pola cat_id" });
+    if (!reg.test(el.amount))
+      return res
+        .status(400)
+        .json({ message: "nieprawidłowy format pola amount" });
+    if (!regShelve.test(el.shelve))
+      return res
+        .status(400)
+        .json({ message: "nieprawidłowy format pola shelve" });
+  });
+
+  req.body.forEach((el) => {
+    let sql = `insert into spareparts (cat_id, amount, shelve) values (${el.cat_id}, ${el.amount}, ${el.shelve})`;
 
     connection.query(sql, (err, result) => {
-        if (err) return res.status(500).json(err)
-        res.status(200).json({ "part_id": result.insertId })
-    })
-})
+      if (err) return console.log(err);
+    });
+  });
+  res.status(200).json({ status: "OK" });
+});
 
-module.exports = router
+module.exports = router;
