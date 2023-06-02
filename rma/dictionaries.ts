05f0e67973db130fc3060b63f6695722574c3b6a
TheWolek <pawelm916@wp.pl>
2023-06-02 17:19:54 +0200

    added rma statuses to dictionary

--- rma/dictionaries.ts
+++ rma/dictionaries.ts
@@ -56,4 +56,31 @@ router.put(
   }
 );
 
+router.get("/statusesTypes", (req: Request, res: Response) => {
+  let query = `SELECT * from tickets_statuses_types`;
+  database.query(query, (err, rows) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(rows);
+  });
+});
+
+router.put(
+  "/statusesTypes",
+  (
+    req: Request<{}, {}, { id: number; newValue: string }, {}>,
+    res: Response
+  ) => {
+    if (req.body.id === undefined || req.body.id === 0)
+      return res.status(400).json({ message: "Pole id jest wymagane" });
+    if (req.body.newValue === undefined || req.body.newValue === "")
+      return res.status(400).json({ message: "Pole newValue jest wymagane" });
+
+    let sql = `UPDATE tickets_statuses_types SET name = '${req.body.newValue}' WHERE id = ${req.body.id}`;
+    database.query(sql, (err, result) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json({});
+    });
+  }
+);
+
 export default router;
